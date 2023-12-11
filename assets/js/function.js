a2643766a2db4fceafaf0f2a7f09ce874aa33f8e
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2023-12-11 22:59:22 +0600

    single item data input and data edit functions have been added

---
+++ assets/js/function.js
@@ -0,0 +1,9 @@
+function getValueFromInput(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputValueString = inputField.value;
+    const inputValue = parseFloat(inputValueString);
+    return inputValue;
+}
+
+// Calculate Area
+
