7f44457042286d16d42a47ea90124f8710ce4f85
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2023-12-11 20:41:01 +0600

    three js file created and in app.js, blog button function is written

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// Header blog button
+const blog_button = document.getElementById('blog-button');
+blog_button.addEventListener('click',function(){
+    console.log('blog clicked');
+    window.location.href = '../dist/blog.html';
+})
+
