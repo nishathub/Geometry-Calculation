b31aa5828949d392c7407c9d92286a87d8180857
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2023-12-12 11:23:46 +0600

    calculate button code is written and shortened by a function

--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -1,1 +1,32 @@
 // selecting the calculate button
+const allCalculateButtons = document.getElementsByClassName('calculate_btn');
+for (calculateButton of allCalculateButtons){
+    calculateButton.addEventListener('click', function(event){
+        // DOM traversing to get to the collected data field
+        const dataDiv = event.target.previousElementSibling; // collected data div
+        const firstDataElement = dataDiv.querySelector('.first-value'); // 
+        const secondDataElement = dataDiv.querySelector('.second-value');
+        const firstDataString = firstDataElement.innerText;
+        const secondDataString = secondDataElement.innerText;
+        const firstData = parseFloat(firstDataString);
+        const secondData = parseFloat(secondDataString);
+        // calculation
+        const itemName = dataDiv.querySelector('.item-name').innerText;
+        let result;
+        if (itemName === 'Triangle'){
+            result = (firstData * secondData) / 2;
+        } else if (itemName === 'Rectangle'){
+            result = (firstData * secondData);
+        } else if (itemName === 'Parallelogram'){
+            result = (firstData * secondData);
+        } else if (itemName === 'Rhombus'){
+            result = (firstData * secondData) / 2;
+        } else if (itemName === 'Pentagon'){
+            result = (firstData * secondData) / 2;
+        } else if (itemName === 'Ellipse'){
+            result = (firstData * secondData) * 3.1416; 
+        }
+        result = result.toFixed(2);
+        addNewListItem (dataDiv, result);
+    })
+}
--- assets/js/function.js
+++ assets/js/function.js
@@ -1,9 +1,28 @@
-function getValueFromInput(inputId){
-    const inputField = document.getElementById(inputId);
-    const inputValueString = inputField.value;
-    const inputValue = parseFloat(inputValueString);
-    return inputValue;
+// AppendChild function
+function addNewListItem (dataDiv, result){
+    const listBox = document.getElementById('result-list');
+    // creating a new list
+    let newListItem = document.createElement('li');
+    // adding classList
+    newListItem.classList.add('flex', 'justify-between', 'px-8', 'items-center');
+    // create Element that will be appended to the newListItem
+    const paragraph = document.createElement('p');
+    const span = document.createElement('span');
+    const button = document.createElement('button');
+    // get item name from data div and set it to the paragraph
+    const itemName = dataDiv.querySelector('.item-name').innerText;
+    paragraph.textContent = itemName;
+    // get result from calculation and set to span
+    span.innerHTML = `${result}cm<sup>2</sup>`
+    // add button classlist 
+    button.classList.add('convert-button');
+    // set button innerText
+    button.innerHTML = `Convert to m<sup>2</sup>`;
+    // add paragraph, span and button to the new list
+    newListItem.appendChild(paragraph);
+    newListItem.appendChild(span);
+    newListItem.appendChild(button);
+    // appendChild newListItem
+    listBox.appendChild(newListItem);
+    
 }
-
-// Calculate Area
-
