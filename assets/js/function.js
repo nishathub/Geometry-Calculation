1cd4b6285578e9ff53b3c379c7f15dec372f0e37
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2023-12-12 11:49:46 +0600

    code added to the function for sequential number in the result box

--- assets/js/function.js
+++ assets/js/function.js
@@ -1,4 +1,5 @@
 // AppendChild function
+let listItemNumber = 2;
 function addNewListItem (dataDiv, result){
     const listBox = document.getElementById('result-list');
     // creating a new list
@@ -6,9 +7,12 @@ function addNewListItem (dataDiv, result){
     // adding classList
     newListItem.classList.add('flex', 'justify-between', 'items-center');
     // create Element that will be appended to the newListItem
+    const numberSpan = document.createElement('span');
     const paragraph = document.createElement('p');
     const span = document.createElement('span');
     const button = document.createElement('button');
+    // set sequential number
+    numberSpan.innerText = listItemNumber + '.';
     // get item name from data div and set it to the paragraph
     const itemName = dataDiv.querySelector('.item-name').innerText;
     paragraph.textContent = itemName;
@@ -19,10 +23,12 @@ function addNewListItem (dataDiv, result){
     // set button innerText
     button.innerHTML = `Convert to m<sup>2</sup>`;
     // add paragraph, span and button to the new list
+    newListItem.appendChild(numberSpan);
     newListItem.appendChild(paragraph);
     newListItem.appendChild(span);
     newListItem.appendChild(button);
     // appendChild newListItem
     listBox.appendChild(newListItem);
+    listItemNumber++;
     
 }
