1b5215503c186a6948e0a45db97dc7dd13d85b8a
Nishat-M <nishat.mohammad19@gmail.comnjfgjfdfd>
2023-12-12 11:59:32 +0600

    calculate if else logic shortened

--- assets/js/calculate.js
+++ assets/js/calculate.js
@@ -2,7 +2,7 @@
 const allCalculateButtons = document.getElementsByClassName('calculate_btn');
 for (calculateButton of allCalculateButtons){
     calculateButton.addEventListener('click', function(event){
-        // DOM traversing to get to the collected data field
+        // DOM traversing to get to the input Data
         const dataDiv = event.target.previousElementSibling; // collected data div
         const firstDataElement = dataDiv.querySelector('.first-value'); // 
         const secondDataElement = dataDiv.querySelector('.second-value');
@@ -11,22 +11,17 @@ for (calculateButton of allCalculateButtons){
         const firstData = parseFloat(firstDataString);
         const secondData = parseFloat(secondDataString);
         // calculation
+        // according to item name, different area calculation will be done
         const itemName = dataDiv.querySelector('.item-name').innerText;
         let result;
-        if (itemName === 'Triangle'){
+        if (itemName === 'Triangle' || itemName === 'Rhombus' || itemName === 'Pentagon'){
             result = (firstData * secondData) / 2;
-        } else if (itemName === 'Rectangle'){
+        } else if (itemName === 'Rectangle' || itemName === 'Parallelogram'){
             result = (firstData * secondData);
-        } else if (itemName === 'Parallelogram'){
-            result = (firstData * secondData);
-        } else if (itemName === 'Rhombus'){
-            result = (firstData * secondData) / 2;
-        } else if (itemName === 'Pentagon'){
-            result = (firstData * secondData) / 2;
         } else if (itemName === 'Ellipse'){
             result = (firstData * secondData) * 3.1416; 
         }
-        result = result.toFixed(2);
-        addNewListItem (dataDiv, result);
+        result = result.toFixed(2); // maximum 2 decimal point
+        addNewListItem (dataDiv, result); // function to appendChild
     })
 }
